e0cce468685f971a3ba081c16d19f199aeae64ff
brightchul <57323359+brightchul@users.noreply.github.com>
2022-04-24 22:58:00 +0900

    chore: lecture, lecture_content sql 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+drop table IF EXISTS lecture; -- member 테이블이 존재할 경우 DROP
+
+create TABLE IF NOT EXISTS lecture ( -- lecture 테이블이 없을 경우 테이블 생성
+    lecture_id INT(20) AUTO_INCREMENT PRIMARY KEY NOT NULL ,
+    lecture_name VARCHAR(50) NOT NULL ,
+    member_id INT(20)  ,
+    lecture_show_yn BOOLEAN ,
+    lecture_total_score INT(10) ,
+    insertDt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    updateDt DATETIME ON update CURRENT_TIMESTAMP
+);
+
+create TABLE IF NOT EXISTS lecture_content (
+    content_id INT(20) AUTO_INCREMENT PRIMARY KEY NOT NULL ,
+    content_name VARCHAR(100) NOT NULL ,
+    content VARCHAR NOT NULL,
+    lecture_id INT(20) NOT NULL,
+    content_type VARCHAR(20) NOT NULL,
+    insertDt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    updateDt DATETIME ON update CURRENT_TIMESTAMP
+)
