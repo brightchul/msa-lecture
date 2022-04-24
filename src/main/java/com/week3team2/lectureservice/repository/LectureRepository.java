4acdae253dfbe926b3b43304efff498baca74bf8
brightchul <57323359+brightchul@users.noreply.github.com>
2022-04-25 00:45:36 +0900

    feat: Lecture 레파지토리 추가

--- src/main/java/com/week3team2/lectureservice/repository/LectureRepository.java
+++ src/main/java/com/week3team2/lectureservice/repository/LectureRepository.java
@@ -1,9 +1,9 @@
 package com.week3team2.lectureservice.repository;
 
-import com.week3team2.lectureservice.model.Lecture;
+import com.week3team2.lectureservice.entity.Lecture;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public interface LectureRepository extends ReactiveCrudRepository<Lecture, Integer> {
 }
